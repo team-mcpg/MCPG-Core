0ef971a1abc7ff224a07738c2488d3297304eaaa
tutur1004 <arthur.barthelme78@gmail.com>
2020-12-31 16:45:11 +0100

    Add - Util to format commands args

---
+++ src/main/java/fr/milekat/MCPG_Core/utils/CmdUtils.java
@@ -0,0 +1,15 @@
+package fr.milekat.MCPG_Core.utils;
+
+public class CmdUtils {
+    public String getArgs(Integer skipedargs, String... args) {
+        StringBuilder sb = new StringBuilder();
+        int loop = 1;
+        for (String string : args) {
+            if (loop < skipedargs) continue;
+            sb.append(string);
+            sb.append(" ");
+            loop++;
+        }
+        return sb.toString();
+    }
+}
