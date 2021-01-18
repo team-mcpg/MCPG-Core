559abe5df628a98487ce18871e819427166f0adc
tutur1004 <arthur.barthelme78@gmail.com>
2021-01-18 14:58:51 +0100

    Add - Add Jackson & mongojack libs - Update MongoClient logins

--- src/main/java/fr/milekat/MCPG_Core/utils/MongoDB.java
+++ src/main/java/fr/milekat/MCPG_Core/utils/MongoDB.java
@@ -1,23 +1,30 @@
 package fr.milekat.MCPG_Core.utils;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import fr.milekat.MCPG_Core.MainCore;
 import org.bukkit.Bukkit;
 
+import java.util.Collections;
+
 public class MongoDB {
-    private final MongoClient mc;
+    private final MongoClient mongoClient;
 
     public MongoDB(String hostname, Long port, String user, String db, String password) {
-        mc = new MongoClient(new ServerAddress(hostname, Math.toIntExact(port)),
-                MongoCredential.createCredential(user, db, password.toCharArray()),
-                MongoClientOptions.builder().build());
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .credential(MongoCredential.createCredential(user, db, password.toCharArray()))
+                .applyToSslSettings(builder -> builder.enabled(true))
+                .applyToClusterSettings(builder -> builder.hosts(Collections.singletonList(
+                        new ServerAddress(hostname, Math.toIntExact(port)))))
+                .build();
+        mongoClient = MongoClients.create(settings);
         Bukkit.getLogger().info(MainCore.prefix + "MongoDB connected !");
     }
 
     public MongoClient getMongoClient() {
-        return mc;
+        return mongoClient;
     }
 }
