c892486fe709b584b467dff3646873c47708e294
tutur1004 <arthur.barthelme78@gmail.com>
2021-02-19 00:37:53 +0100

    Add - getBool function to get true/false from Bukkit command arg

--- src/main/java/fr/milekat/MCPG_Core/utils/CmdUtils.java
+++ src/main/java/fr/milekat/MCPG_Core/utils/CmdUtils.java
@@ -1,5 +1,9 @@
 package fr.milekat.MCPG_Core.utils;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Locale;
+
 public class CmdUtils {
     public static String getArgs(Integer skipedargs, String... args) {
         StringBuilder sb = new StringBuilder();
@@ -13,4 +17,14 @@ public class CmdUtils {
         return sb.toString();
     }
 
+    /**
+     * Get true/false from Bukkit command arg
+     */
+    public static ArrayList<String> getBool(String arg) {
+        if ("true".startsWith(arg.toLowerCase(Locale.ROOT))) {
+            return new ArrayList<>(Collections.singletonList("true"));
+        } else if ("false".startsWith(arg.toLowerCase(Locale.ROOT))) {
+            return new ArrayList<>(Collections.singletonList("false"));
+        } else return null;
+    }
 }
